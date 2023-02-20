6b1c13bd26e33c18daec63c313ba0ffd9d13ed99
VTYTaquitos <flujanmonroy@gmail.com>
2023-02-20 16:59:37 -0600

    Se le agrego metodo del Personaja correr, lanzar y recarga

--- Personaje.py
+++ Personaje.py
@@ -4,3 +4,18 @@ class Personaje:
     especie = "Humano"
     nombre = "Jerry"
     altura = "2.80"
+    
+    #Metodo Personaje 
+    def correr(self,status):
+        if(status):
+            print("el personaje"+ self.nombre + " esta corriendo")
+        else:
+            print("el personaje"+ self.nombre + " se detuvo")
+
+    def lanzarGranadas(self):
+        print("el personaje"+ self.nombre + " lanzo una granada")
+    
+    def recargarArma(self,municion):
+        cargador = 10
+        cargador = cargador + municion
+        print("el arma tiene"+ cargador + "balas")
