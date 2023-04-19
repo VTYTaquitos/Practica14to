eaba0464b64a0155404e8a45aab753cf37b912d2
VTYTaquitos <flujanmonroy@gmail.com>
2023-04-19 17:04:57 -0600

    Se finalizo el examen de 3er Parcial del ejerccicio del banco

--- Examen3P/ControlBD.py
+++ Examen3P/ControlBD.py
@@ -9,37 +9,35 @@ class controladorBD:
     def conexionBD(self):
         try: 
             conexion = sqlite3.connect("C:/Users/frank/OneDrive/Documents/GitHub/Practica14to/Examen3P/BD_Banco.db")
-        
+            return conexion
         except sqlite3.OperationalError:
             print("No se pudo conectar")
             
-    def InsertarBD(self,nom,cue,con):
+    def InsertarBD(self,cue,sal):
         conx = self.conexionBD()
     
-        if (nom == "" or cue =="" or con==""):
+        if (cue =="" or sal==""):
             messagebox.showwarning("Cuidado","Revisa lo que pusiste")
             conx.close()
         else:
             cursor = conx.cursor()
-            datos = (nom,cue,con)
-            qrInster = "insert into TBCunetas (Nombre,NoCuenta,Contra) values (?,?,?)" 
+            datos = (cue,sal)
+            qrInster = "insert into TBCuentas (NoCuenta,Saldo) values (?,?)" 
             cursor.execute(qrInster,datos)
             conx.commit()
             conx.close()
             messagebox.showinfo("Bien","Se ah insertado correctamente el usuario")
     
     
-    def CambiarInfo(self,idE,nomE,cueE,conE):
+    def CambiarInfo(self,idE,cueE,salE):
         conx = self.conexionBD()
-        if (idE == "" or nomE == "" or cueE =="" or conE==""):
+        if (idE == "" or cueE =="" or salE==""):
             messagebox.showwarning("Cuidado","Revisa lo que pusiste")
             conx.close()
         else:
             cursor = conx.cursor()
-            conH = self.encriptarCon(conE)
-            #datos = (nomE,corE,conH)
-            datos = (nomE,cueE,conH,idE)
-            qrActualizar = ("UPDATE TBCunetas SET Nombre = ?, NoCuenta = ?, Contra = ? WHERE Id = ?")
+            datos = (cueE,salE,idE)
+            qrActualizar = ("UPDATE TBCuentas SET NoCuenta = ?, Saldo = ? WHERE Id_Cuneta = ?")
             cursor.execute(qrActualizar,datos)
             conx.commit()
             conx.close()
@@ -49,7 +47,7 @@ class controladorBD:
         conx = self.conexionBD()
         try:
              cursor=conx.cursor()
-             sqlMostrar = "select * from TBCunetas"
+             sqlMostrar = "select * from TBCuentas"
              cursor.execute(sqlMostrar)
              MResu=cursor.fetchall()
              conx.close()
--- Examen3P/ventana.py
+++ Examen3P/ventana.py
@@ -7,10 +7,10 @@ from ControlBD import *
 labasedatos = controladorBD()
 
 def Instertar():
-    labasedatos.InsertarBD(VarNom.get(),VarCuenta.get(),VarContra.get())
+    labasedatos.InsertarBD(str(VarCuenta.get()),str(VarSaldo.get()))
 
 def ModificarUsuario():        
-    labasedatos.CambiarInfo(VarIDC.get(),VarNomC.get(),VarCuentaC.get(),VarContraC.get())
+    labasedatos.CambiarInfo(str(VarIDC.get()),str(VarCuentaC.get()),str(VarSaldoC.get()))
 
 def MostrarUsuarios():   
     usuarios = labasedatos.mostrarUsuarios()
@@ -33,15 +33,12 @@ pestana3= ttk.Frame(panel)
 
 #Ventana Insertar 
 titulo = Label(pestana1,text="Insterar de usuario",fg="blue",font= ("Modern",18)).pack()
-VarNom=tk.StringVar()
-lblNom=Label(pestana1,text="Nombre:").pack()
-txtNom = Entry(pestana1,textvariable= VarNom).pack()
 VarCuenta=tk.StringVar()
 lblCorreo=Label(pestana1,text="No Cuenta:").pack()
 txtCorreo = Entry(pestana1,textvariable= VarCuenta).pack()
-VarContra=tk.StringVar()
-lblContra=Label(pestana1,text="Contraseña:").pack()
-txtContra = Entry(pestana1,textvariable= VarContra).pack()
+VarSaldo=tk.StringVar()
+lblContra=Label(pestana1,text="Saldo:").pack()
+txtContra = Entry(pestana1,textvariable= VarSaldo).pack()
 btnGuardar = Button(pestana1, text="Guardar",command= Instertar).pack()
 
 #Actualizar
@@ -49,28 +46,25 @@ titulo = Label(pestana2,text="Modificar usuario",fg="blue",font= ("Modern",18)).
 VarIDC=tk.StringVar()
 lblIDC=Label(pestana2,text="Id:").pack()
 txtIDC = Entry(pestana2,textvariable= VarIDC).pack()
-VarNomC=tk.StringVar()
-lblNomC=Label(pestana2,text="Nombre:").pack()
-txtNomC = Entry(pestana2,textvariable= VarNomC).pack()
 VarCuentaC=tk.StringVar()
 lblCorreoC=Label(pestana2,text="Cuenta:").pack()
 txtCorreoC = Entry(pestana2,textvariable= VarCuentaC).pack()
-VarContraC=tk.StringVar()
-lblContraC=Label(pestana2,text="Contraseña:").pack()
-txtContraC = Entry(pestana2,textvariable= VarContraC).pack()
+VarSaldoC=tk.StringVar()
+lblContraC=Label(pestana2,text="Saldo:").pack()
+txtContraC = Entry(pestana2,textvariable= VarSaldoC).pack()
 btnMostrar = Button(pestana2, text="Actualizar",command=ModificarUsuario).pack()
 
 
 #Mostrar Todo
 titulo = Label(pestana3,text="Mostrar usuarios",fg="blue",font= ("Modern",18)).pack()
-columnas = ('Id','Nombre','Cuenta') 
+columnas = ('Id','Cuenta','Saldo') 
 tablaM = ttk.Treeview(pestana3,columns= columnas,show="headings")
 tablaM.column("Id", width=100)
-tablaM.column("Nombre", width=100)
 tablaM.column("Cuenta", width=100)
+tablaM.column("Saldo", width=100)
 tablaM.heading("Id",text="ID")
-tablaM.heading("Nombre",text="NOMBRE")
 tablaM.heading("Cuenta",text="NOCUENTA")
+tablaM.heading("Saldo",text="Saldo")
 tablaM.pack()
 btnMostrar = Button(pestana3, text="Mostrar",command=MostrarUsuarios).pack()
 
